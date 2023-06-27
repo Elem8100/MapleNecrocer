ed97008305f8d2b3a1a4a41706471609e9f95df3
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2023-06-27 22:55:56 +0800

    medal form

--- MapleNecrocer/MedalForm.Designer.cs
+++ MapleNecrocer/MedalForm.Designer.cs
@@ -28,22 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Location = new Point(12, 43);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(335, 557);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label1.Location = new Point(12, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 18);
+            label1.TabIndex = 1;
+            label1.Text = "Search";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(65, 15);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(116, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(234, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 28);
+            button1.TabIndex = 3;
+            button1.Text = "Remove";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // MedalForm
             // 
             AutoScaleMode = AutoScaleMode.None;
-            ClientSize = new Size(342, 612);
+            ClientSize = new Size(359, 612);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
             Font = new Font("Tahoma", 13F, FontStyle.Regular, GraphicsUnit.Pixel);
-            MaximumSize = new Size(360, 900);
+            KeyPreview = true;
+            MaximumSize = new Size(377, 900);
             Name = "MedalForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "MedalForm";
+            Text = "Medal";
             TopMost = true;
             Shown += MedalForm_Shown;
+            KeyDown += MedalForm_KeyDown;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
--- MapleNecrocer/MedalForm.cs
+++ MapleNecrocer/MedalForm.cs
@@ -22,6 +22,13 @@ public partial class MedalForm : Form
     public static MedalForm Instance;
     public DataGridViewEx MedalListGrid;
 
+    void CellClick(BaseDataGridView DataGrid, DataGridViewCellEventArgs e)
+    {
+        MedalTag.Delete();
+        var ID = DataGrid.Rows[e.RowIndex].Cells[0].Value.ToString();
+        MedalTag.Create(ID);
+
+    }
     private void MedalForm_Shown(object sender, EventArgs e)
     {
 
@@ -31,15 +38,26 @@ public partial class MedalForm : Form
             e1.Cancel = true;
         };
 
-        MedalListGrid = new(90, 164, 0, 0, 220, 400, true, this);
+        MedalListGrid = new(90, 164, 0, 0, 220, 400, true, panel1);
         MedalListGrid.Dock = DockStyle.Fill;
         MedalListGrid.SearchGrid.Dock = DockStyle.Fill;
+        MedalListGrid.RowTemplate.Height = 40;
 
         var Graphic = MedalListGrid.CreateGraphics();
         var Font = new System.Drawing.Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold);
         Graphic.DrawString("Loading...", Font, Brushes.Black, 10, 50);
+
+        MedalListGrid.CellClick += (s, e) =>
+        {
+            CellClick(MedalListGrid, e);
+        };
+
+        MedalListGrid.SearchGrid.CellClick += (s, e) =>
+        {
+            CellClick(MedalListGrid.SearchGrid, e);
+        };
         string MedalName = null;
-        Bitmap Bmp=null;
+        Bitmap Bmp = null;
         foreach (var Img in Wz.GetNode("Character/Accessory").Nodes)
         {
             if (Img.Text.LeftStr(4) != "0114")
@@ -52,11 +70,35 @@ public partial class MedalForm : Form
             string ID = Img.ImgID();
             if (Wz.HasNode("String/Eqp.img/Eqp/Accessory/" + ID.IntID()))
                 MedalName = Wz.GetNode("String/Eqp.img/Eqp/Accessory/" + ID.IntID() + "/name").ToStr();
-            if( Wz.HasNode("Character/Accessory/" + Img.Text + "/info/icon"))
-                Bmp= Wz.GetNode("Character/Accessory/" + Img.Text + "/info/icon").ExtractPng();
+            if (Wz.HasNode("Character/Accessory/" + Img.Text + "/info/icon"))
+                Bmp = Wz.GetNode("Character/Accessory/" + Img.Text + "/info/icon").ExtractPng();
             MedalListGrid.Rows.Add(ID, Bmp, MedalName);
-
         }
 
+        for (int i = 0; i < MedalListGrid.Rows.Count; i++)
+        {
+            MedalListGrid.Rows[i].Cells[1].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            MedalListGrid.Rows[i].Cells[2].Style.Alignment = DataGridViewContentAlignment.TopLeft;
+        }
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        MedalTag.Delete();
+    }
+
+    private void MedalForm_KeyDown(object sender, KeyEventArgs e)
+    {
+
+        if (e.Alt)
+            e.Handled = true;
+        if (!textBox1.Focused)
+            ActiveControl = null;
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        MedalListGrid.Search(textBox1.Text);
     }
 }
