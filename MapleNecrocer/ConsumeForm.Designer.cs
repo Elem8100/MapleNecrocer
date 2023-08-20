6838caa150f1a94f6c8c1da75746a7e79d65b562
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2023-08-20 19:33:32 +0800

    Consume Form

--- MapleNecrocer/ConsumeForm.Designer.cs
+++ MapleNecrocer/ConsumeForm.Designer.cs
@@ -31,7 +31,19 @@
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
+            textBox1 = new TextBox();
+            tabPage3 = new TabPage();
+            panel1 = new Panel();
+            UseButton = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
             tabControl1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // tabControl1
@@ -39,44 +51,132 @@
             tabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(6, 29);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(6, 58);
             tabControl1.Name = "tabControl1";
+            tabControl1.Padding = new Point(6, 0);
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(420, 592);
+            tabControl1.Size = new Size(420, 595);
             tabControl1.TabIndex = 7;
+            tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 26);
+            tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(412, 562);
+            tabPage1.Size = new Size(412, 567);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Icons";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 28);
+            tabPage2.Controls.Add(textBox1);
+            tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(412, 560);
+            tabPage2.Padding = new Padding(3, 3, 3, 5);
+            tabPage2.Size = new Size(412, 567);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Search";
             tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(162, 24);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(button1);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(412, 567);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Consume Effect";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(UseButton);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(56, 5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(320, 49);
+            panel1.TabIndex = 8;
+            // 
+            // UseButton
+            // 
+            UseButton.Location = new Point(263, 6);
+            UseButton.Name = "UseButton";
+            UseButton.Size = new Size(52, 35);
+            UseButton.TabIndex = 3;
+            UseButton.Text = "Drop";
+            UseButton.TextAlign = ContentAlignment.MiddleLeft;
+            UseButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(74, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(40, 40);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 18);
+            label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(110, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 18);
+            label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(120, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 27);
+            button1.TabIndex = 0;
+            button1.Text = "Remove";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // ConsumeForm
             // 
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(432, 650);
+            Controls.Add(panel1);
             Controls.Add(tabControl1);
             Font = new Font("Tahoma", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
             KeyPreview = true;
             Name = "ConsumeForm";
+            Padding = new Padding(0, 0, 0, 5);
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Consume";
             TopMost = true;
             Shown += ConsumeForm_Shown;
             tabControl1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -85,5 +185,13 @@
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private TextBox textBox1;
+        private TabPage tabPage3;
+        private Panel panel1;
+        private Button UseButton;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
     }
 }
--- MapleNecrocer/ConsumeForm.cs
+++ MapleNecrocer/ConsumeForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Manina.Windows.Forms;
+using WzComparerR2.CharaSim;
 
 namespace MapleNecrocer;
 
@@ -20,7 +21,9 @@ public partial class ConsumeForm : Form
     }
     ImageListView ImageGrid;
     public static ConsumeForm Instance;
-
+    DataGridViewEx ConsumeListGrid;
+    DataGridViewEx ConsumeEffectListGrid;
+    bool HasLoaded1, HasLoaded2;
     private void ConsumeForm_Shown(object sender, EventArgs e)
     {
         this.FormClosing += (s, e1) =>
@@ -42,9 +45,12 @@ public partial class ConsumeForm : Form
         //ImageGrid.t.t.show.CacheMode= CacheMode.Continuous;
         ImageGrid.ItemClick += (o, e) =>
         {
-            Text = (e.Item.FileName);
-
+            string ID = (e.Item.FileName);
+            label1.Text = ID;
+            pictureBox1.Image = Wz.GetBmp("Item/Consume/" + ID.LeftStr(4) + ".img/" + ID + "/info/icon");
+            label2.Text = Wz.GetStr("String/Consume.img/" + ID.IntID() + "/name");
         };
+
 
 
         var Graphic = ImageGrid.CreateGraphics();
@@ -66,5 +72,139 @@ public partial class ConsumeForm : Form
         ImageGrid.Sort();
         Win32.SendMessage(ImageGrid.Handle, true);
         ImageGrid.Refresh();
+    }
+
+    private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        label1.Text = "";
+        pictureBox1.Image = null;
+        label2.Text = "";
+        if (tabControl1.SelectedIndex == 1)
+        {
+            if (HasLoaded1)
+                return;
+
+
+            ConsumeListGrid = new(80, 185, 0, 20, 220, 530, true, tabControl1.TabPages[1]);
+            ConsumeListGrid.Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
+            ConsumeListGrid.Dock = DockStyle.Fill;
+            tabControl1.TabPages[1].Padding = new Padding(0, 35, 0, 0);
+            ConsumeListGrid.SearchGrid.Dock = DockStyle.Fill;
+            ConsumeListGrid.RowTemplate.Height = 40;
+
+            var Graphic = ConsumeListGrid.CreateGraphics();
+            var Font = new System.Drawing.Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold);
+            Graphic.DrawString("Loading...", Font, Brushes.Black, 10, 50);
+            void CellClick(BaseDataGridView DataGrid, DataGridViewCellEventArgs e)
+            {
+                string ID = DataGrid.Rows[e.RowIndex].Cells[0].Value.ToString();
+                label1.Text = ID;
+                pictureBox1.Image = (Bitmap)ConsumeListGrid.Rows[e.RowIndex].Cells[1].Value;
+                label2.Text = ConsumeListGrid.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+
+            ConsumeListGrid.CellClick += (s, e) =>
+            {
+                CellClick(ConsumeListGrid, e);
+            };
+
+            ConsumeListGrid.SearchGrid.CellClick += (s, e) =>
+            {
+                CellClick(ConsumeListGrid.SearchGrid, e);
+            };
+
+            Win32.SendMessage(ConsumeListGrid.Handle, false);
+            Bitmap Bmp = null;
+            string ConsumeName = "";
+            foreach (var Img in Wz.GetNodes("Item/Consume"))
+            {
+                if (!Char.IsNumber(Img.Text[0]))
+                    continue;
+                foreach (var Iter in Wz.GetNodes("Item/Consume/" + Img.Text))
+                {
+                    string ID = Iter.Text;
+                    string IntID = ID.IntID();
+                    if (Wz.HasNode("String/Consume.img/" + IntID))
+                        ConsumeName = Wz.GetStr("String/Consume.img/" + IntID + "/name");
+                    if (Iter.HasNode("info/icon"))
+                        Bmp = Iter.GetBmp("info/icon");
+                    ConsumeListGrid.Rows.Add(ID, Bmp, ConsumeName);
+                }
+            }
+
+            Win32.SendMessage(ConsumeListGrid.Handle, true);
+            ConsumeListGrid.Refresh();
+            for (int i = 0; i < ConsumeListGrid.Rows.Count; i++)
+            {
+                ConsumeListGrid.Rows[i].Cells[1].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                ConsumeListGrid.Rows[i].Cells[2].Style.Alignment = DataGridViewContentAlignment.TopLeft;
+            }
+            HasLoaded1 = true;
+        }
+
+        if (tabControl1.SelectedIndex == 2)
+        {
+            if (HasLoaded2)
+                return;
+            ConsumeEffectListGrid = new(80, 185, 0, 20, 220, 530, true, tabControl1.TabPages[2]);
+            ConsumeEffectListGrid.Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
+            ConsumeEffectListGrid.Dock = DockStyle.Fill;
+            tabControl1.TabPages[2].Padding = new Padding(0, 35, 0, 0);
+            ConsumeEffectListGrid.SearchGrid.Dock = DockStyle.Fill;
+            ConsumeEffectListGrid.RowTemplate.Height = 40;
+            var Graphic = ConsumeEffectListGrid.CreateGraphics();
+            var Font = new System.Drawing.Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold);
+            Graphic.DrawString("Loading...", Font, Brushes.Black, 10, 50);
+            void CellClick(BaseDataGridView DataGrid, DataGridViewCellEventArgs e)
+            {
+                string ID = DataGrid.Rows[e.RowIndex].Cells[0].Value.ToString();
+                ItemEffect.Delete(EffectType.Consume);
+                ItemEffect.Create(ID, EffectType.Consume);
+
+            }
+
+            ConsumeEffectListGrid.CellClick += (s, e) =>
+            {
+                CellClick(ConsumeEffectListGrid, e);
+            };
+
+
+            Win32.SendMessage(ConsumeEffectListGrid.Handle, false);
+            Bitmap Bmp = null;
+            string ConsumeEffectName = "";
+            foreach (var Iter in Wz.GetNodes("Effect/ItemEff.img"))
+            {
+                if (Iter.Text[0] == '2')
+                {
+                    string ID = '0' + Iter.Text;
+                    if (Wz.HasNode("Item/Consume/" + ID.LeftStr(4) + ".img/" + ID + "/info/icon"))
+                    {
+                        Bmp = Wz.GetBmp("Item/Consume/" + ID.LeftStr(4) + ".img/" + ID + "/info/icon");
+
+                    }
+                    ConsumeEffectName = Wz.GetStr("String/Consume.img/" + ID.IntID() + "/name");
+                    ConsumeEffectListGrid.Rows.Add(ID, Bmp, ConsumeEffectName);
+                }
+            }
+            Win32.SendMessage(ConsumeEffectListGrid.Handle, true);
+            ConsumeEffectListGrid.Refresh();
+            for (int i = 0; i < ConsumeEffectListGrid.Rows.Count; i++)
+            {
+                ConsumeEffectListGrid.Rows[i].Cells[1].Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                ConsumeEffectListGrid.Rows[i].Cells[2].Style.Alignment = DataGridViewContentAlignment.TopLeft;
+            }
+            HasLoaded2 = true;
+        }
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        ConsumeListGrid.Search(textBox1.Text);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        ItemEffect.Delete(EffectType.Consume);
     }
 }
