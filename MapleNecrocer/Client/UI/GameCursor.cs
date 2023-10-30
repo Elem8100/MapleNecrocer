7f9aea72917d577f5a6ae599b82676c65f2d144f
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2023-10-31 02:22:35 +0800

    game cursor

---
+++ MapleNecrocer/Client/UI/GameCursor.cs
@@ -0,0 +1,71 @@
+﻿using WzComparerR2.WzLib;
+using MapleNecrocer;
+using MouseExt;
+using Microsoft.Xna.Framework.Input;
+using System.Security.Cryptography.X509Certificates;
+using ButtonState=Microsoft.Xna.Framework.Input.ButtonState;
+namespace GameUI;
+
+public class GameCursor
+{
+    public static bool IsDataWz;
+    static int Frame;
+    static string CursorNumber="0";
+    static int FTime;
+    static Wz_Node ImageNode;
+    static bool HasAnim;
+    //static Wz_Node ImagEntry;
+    public static void LoadRes(string CursorNum)
+    {
+        Wz.DumpData(Wz.GetNode("UI/Basic.img/Cursor/" + CursorNum), Wz.UIData, Wz.UIImageLib);
+    }
+
+    public static void Draw()
+    {
+        var MouseState = MouseEx.GetState();
+        if(MouseState.LeftButton== ButtonState.Pressed)
+        { 
+           if(!IsDataWz)
+            Change("12");
+        }
+        
+        if (MouseState.LeftButton == ButtonState.Released)
+        { 
+            Change("0");
+        }
+
+        if (Wz.UIData.ContainsKey("UI/Basic.img/Cursor/" + CursorNumber + "/1"))
+        {
+            ImageNode = Wz.UIData["UI/Basic.img/Cursor/" + CursorNumber + '/' + Frame];
+            int Delay = ImageNode.GetInt("delay", 100);
+            FTime += 17;
+            if (FTime > Delay)
+            {
+                Frame += 1;
+                if (!Wz.UIData.ContainsKey("UI/Basic.img/Cursor/" + CursorNumber + '/' + Frame))
+                    Frame = 0;
+                FTime = 0;
+            }
+            Wz_Vector Origin = ImageNode.GetVector("origin");
+            int OffsetX = -Origin.X + 3;
+            int OffsetY = -Origin.Y + 3;
+            EngineFunc.Canvas.Draw(Wz.UIImageLib[ImageNode], MouseState.X + OffsetX, MouseState.Y + OffsetY);
+        }
+        else
+        {
+            ImageNode = Wz.UIData["UI/Basic.img/Cursor/" + CursorNumber + "/0"];
+            Wz_Vector Origin = ImageNode.GetVector("origin");
+            int OffsetX = -Origin.X + 3;
+            int OffsetY = -Origin.Y + 3;
+            EngineFunc.Canvas.Draw(Wz.UIImageLib[ImageNode], MouseState.X + OffsetX, MouseState.Y + OffsetY);
+        }
+    }
+
+    public static void Change(string Number)
+    {
+        if (Frame != 0)
+            Frame = 0;
+        CursorNumber = Number;
+    }
+
+}
--- MapleNecrocer/Client/UI/UI.Utils.cs
+++ MapleNecrocer/Client/UI/UI.Utils.cs
@@ -11,6 +11,7 @@ using Microsoft.Xna.Framework;
 using MonoGame.UI.Forms;
 using MapleNecrocer;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 
 namespace GameUI;
 public class UIControlManager : ControlManager
@@ -26,14 +27,15 @@ public class UI
     public static UIControlManager ControlManager = new();
     public static UIForm RefForm;
 
-    public static Dictionary<string, UIForm> Form=new();
+    public static Dictionary<string, UIForm> Form = new();
+    public static Dictionary<string, UIButton> Button = new();
     public static void CreateDefault()
     {
 
 
     }
 
-    public static void CrateForm(string UIPath, int X = 0, int Y = 0, bool IsMoveable = true,bool visible=true)
+    public static void CrateForm(string UIPath, int X = 0, int Y = 0, bool IsMoveable = true, bool visible = true)
     {
         var _Form = new UIForm();
         // Form.HitBox=new Microsoft.Xna.Framework.Rectangle(0,0,500,500);
@@ -45,7 +47,7 @@ public class UI
         _Form.Size = new Vector2(UIPng.Width, UIPng.Height);
         _Form.IsMovable = IsMoveable;
         _Form.IsVisible = visible;
-       
+
         RefForm = _Form;
         ControlManager.Controls.Add(_Form);
         Form.Add(UIPath, _Form);
@@ -54,14 +56,33 @@ public class UI
     {
         var Button = new UIButton();
         var UINode = Wz.GetNode(UIPath);
-        Wz.DumpData(UINode, Wz.UIData, Wz.UIImageLib);
+        if (!Wz.UIData.ContainsKey(UINode.FullPathToFile2()))
+            Wz.DumpData(UINode, Wz.UIData, Wz.UIImageLib);
         Button.UIPath = UIPath;
-        Button.Location = new Vector2(X, Y);
+
+        var Origin = UINode.GetVector("normal/0/ogigin");
+        Button.Location = new Vector2(X + -Origin.X, Y + -Origin.Y);
         var UIPng = UINode.GetBmp("normal/0");
         Button.Size = new Vector2(UIPng.Width, UIPng.Height);
         Button.Clicked += (s, e) => { ClickEvent(); };
         RefForm.Controls.Add(Button);
+    }
 
+    public static void CreateButton(string ButtonName, string UIPath, int X = 0, int Y = 0, Action ClickEvent = null)
+    {
+        var _Button = new UIButton();
+        var UINode = Wz.GetNode(UIPath);
+        if (!Wz.UIData.ContainsKey(UINode.FullPathToFile2()))
+            Wz.DumpData(UINode, Wz.UIData, Wz.UIImageLib);
+        _Button.UIPath = UIPath;
+
+        var Origin = UINode.GetVector("normal/0/ogigin");
+        _Button.Location = new Vector2(X + -Origin.X, Y + -Origin.Y);
+        var UIPng = UINode.GetBmp("normal/0");
+        _Button.Size = new Vector2(UIPng.Width, UIPng.Height);
+        _Button.Clicked += (s, e) => { ClickEvent(); };
+        RefForm.Controls.Add(_Button);
+        Button.Add(ButtonName, _Button);
     }
 
 }
