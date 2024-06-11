9d7b5562d42e67b46f2e559e1c1b7372d1a1f944
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2024-06-11 19:32:31 +0800

    Added chat ring

---
+++ MapleNecrocer/ChatRingForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MapleNecrocer
+{
+    partial class ChatRingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Location = new Point(14, 147);
+            panel1.Margin = new Padding(5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(364, 614);
+            panel1.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(14, 18);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(364, 86);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "MapleStory";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 18);
+            label1.TabIndex = 10;
+            label1.Text = "Search";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(77, 116);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(143, 24);
+            textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(276, 115);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 24);
+            button1.TabIndex = 12;
+            button1.Text = "Remove";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // ChatRingForm
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(392, 775);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(panel1);
+            Font = new Font("Tahoma", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            KeyPreview = true;
+            Name = "ChatRingForm";
+            Text = "ChatRingForm";
+            TopMost = true;
+            FormClosing += ChatRingForm_FormClosing;
+            Shown += ChatRingForm_Shown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ MapleNecrocer/ChatRingForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapleNecrocer;
+
+public partial class ChatRingForm : Form
+{
+    public ChatRingForm()
+    {
+        InitializeComponent();
+        Instance = this;
+    }
+    public static ChatRingForm Instance;
+    public DataGridViewEx ChatRingListGrid;
+
+    void CellClick(BaseDataGridView DataGrid, DataGridViewCellEventArgs e)
+    {
+        var ID = DataGrid.Rows[e.RowIndex].Cells[0].Value.ToString();
+        if (ID == "01112243")
+            return;
+        ChatRingBalloon.Remove();
+        var chatRingBalloon = new ChatRingBalloon(EngineFunc.SpriteEngine);
+        //  chatRingBalloon.Msg = richTextBox1.Text;
+        if (Wz.Region == "GMS")
+        {
+            chatRingBalloon.Msg = richTextBox1.Text;
+            //Npc.Msgs.Add(Str1);
+        }
+        else
+        {
+            string Str1 = chatRingBalloon.Msg = richTextBox1.Text;
+            Str1 = Str1.Replace(" ", "");
+            chatRingBalloon.Msg = Npc.ReString(Str1);
+        }
+
+        int TagNum = Wz.GetInt("Character/Ring/" + ID + ".img" + "/info/chatBalloon");
+        chatRingBalloon.SetStyle(TagNum);
+        chatRingBalloon.ReDraw();
+
+    }
+    private void ChatRingForm_Shown(object sender, EventArgs e)
+    {
+        this.FormClosing += (s, e1) =>
+        {
+            this.Hide();
+            e1.Cancel = true;
+        };
+
+        ChatRingListGrid = new(90, 179, 0, 0, 220, 400, true, panel1);
+        ChatRingListGrid.Dock = DockStyle.Fill;
+        ChatRingListGrid.SearchGrid.Dock = DockStyle.Fill;
+        ChatRingListGrid.RowTemplate.Height = 40;
+
+        var Graphic = ChatRingListGrid.CreateGraphics();
+        var Font = new System.Drawing.Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold);
+        Graphic.DrawString("Loading...", Font, Brushes.Black, 10, 50);
+        ChatRingListGrid.CellClick += (s, e) =>
+        {
+            CellClick(ChatRingListGrid, e);
+        };
+
+        ChatRingListGrid.SearchGrid.CellClick += (s, e) =>
+        {
+            CellClick(ChatRingListGrid.SearchGrid, e);
+        };
+        ChatRingListGrid.SetToolTipEvent(WzType.Character, this);
+
+        foreach (var Img in Wz.GetNodes("Character/Ring"))
+        {
+            if (Img.Text.LeftStr(6) != "011122" && Img.Text.LeftStr(6) != "011150" && Img.Text.LeftStr(6) != "011152")
+                continue;
+
+            if (!Wz.HasNode("Character/Ring/" + Img.Text + "/info/chatBalloon"))
+                continue;
+            int TagNum = Wz.GetInt("Character/Ring/" + Img.Text + "/info/chatBalloon");
+
+            if (!Wz.HasNode("UI/ChatBalloon.img/" + TagNum))
+                continue;
+
+            string ID = Img.ImgID();
+            string ChatRingName = "";
+
+            if (Wz.HasNode("String/Eqp.img/Eqp/Ring/" + ID.ToInt()))
+                ChatRingName = Wz.GetStr("String/Eqp.img/Eqp/Ring/" + ID.ToInt() + "/name");
+
+            Bitmap Icon = null;
+            if (Wz.HasNode("Character/Ring/" + ID + ".img/info/icon"))
+                Icon = Wz.GetBmp("Character/Ring/" + ID + ".img/info/icon");
+            ChatRingListGrid.Rows.Add(ID, Icon, ChatRingName);
+
+        }
+
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        ChatRingBalloon.Remove();
+    }
+
+    private void ChatRingForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        MainForm.Instance.ToolTipView.Visible = false;
+    }
+}
