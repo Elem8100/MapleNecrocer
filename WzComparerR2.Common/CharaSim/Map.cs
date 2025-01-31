6d64670d6496c0d8327968989f0ad121b3639a55
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2025-01-31 15:11:57 +0800

    Map's ToolTip

---
+++ WzComparerR2.Common/CharaSim/Map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WzComparerR2.WzLib;
+
+namespace WzComparerR2.CharaSim
+{
+    public class Map
+    {
+        public static Wz_Node ImgNode;
+    }
+
+}
